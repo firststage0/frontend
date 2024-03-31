76817035d0b0197d8b3ee42ed89c36be20796288
firststage0 <red_tk@mail.ru>
2024-04-01 00:17:39 +0300

    add json file

--- weatherService/app.js
+++ weatherService/app.js
@@ -1,5 +1,17 @@
+const request = new XMLHttpRequest();
+request.open("GET", "/cities.json");
+request.responseType = "json";
+request.send();
+var citiesJson;
+request.onload = () => {
+    citiesJson = request.response;
+    return citiesJson;
+}
+
+console.log(citiesJson);
 const apiKey = "57f7df1e3063971e738d4e9c5af1bb15";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Voronezh&appid="+apiKey+"&units=metric";
+const cityForRequest = "Saint Petersburg, RU";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityForRequest+"&appid="+apiKey+"&units=metric";
 const weatherDiv = document.getElementById("forWeather");
 fetch(apiURL)
     .then(res => {
@@ -10,7 +22,7 @@ fetch(apiURL)
     })
     .then(data => {
         console.log(data);
-        forWeather.innerHTML = "Current weather in this city is: " + data.main.temp;
+        forWeather.innerHTML = "Current weather in " + cityForRequest + " is: " + data.main.temp;
     })
     .catch(error => {
         console.error("Error: ", error);
