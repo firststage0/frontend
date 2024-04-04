7b38a8cfe2be020f8191a88d8cd7bf3884fffc8b
firststage0 <red_tk@mail.ru>
2024-04-04 11:20:25 +0300

    buttons have been added

--- weatherService/app.js
+++ weatherService/app.js
@@ -1,29 +1,42 @@
-const request = new XMLHttpRequest();
-request.open("GET", "/cities.json");
-request.responseType = "json";
-request.send();
-var citiesJson;
-request.onload = () => {
-    citiesJson = request.response;
-    return citiesJson;
+// const request = new XMLHttpRequest();
+// request.open("GET", "/cities.json");
+// request.responseType = "json";
+// request.send();
+// var citiesJson;
+// request.onload = () => {
+//     citiesJson = request.response;
+//     return citiesJson;
+// }
+const apiKey = "57f7df1e3063971e738d4e9c5af1bb15";
+const citiesData = {
+    "Москва": "Moscow, RU",
+    "Санкт-петербург": "Saint Petersburg, RU",
+    "Воронеж": "Voronezh, RU",
+    "Самара": "Samara, RU"
+}
+function setCityRequest(city) {
+
+    const cityForRequest = citiesData[city];
+    executeResponse(cityForRequest);
+    console.log(cityForRequest);
+    return;
 }
 
-console.log(citiesJson);
-const apiKey = "57f7df1e3063971e738d4e9c5af1bb15";
-const cityForRequest = "Saint Petersburg, RU";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityForRequest+"&appid="+apiKey+"&units=metric";
-const weatherDiv = document.getElementById("forWeather");
-fetch(apiURL)
-    .then(res => {
-        if(!res.ok){
-            throw new Error("Response is'nt ok");
-        }
-        return res.json();
-    })
-    .then(data => {
-        console.log(data);
-        forWeather.innerHTML = "Current weather in " + cityForRequest + " is: " + data.main.temp;
-    })
-    .catch(error => {
-        console.error("Error: ", error);
-    })
+function executeResponse(cityForRequest) {
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityForRequest + "&appid=" + apiKey + "&units=metric";
+    const weatherDiv = document.getElementById("forWeather");
+    fetch(apiURL)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Response is'nt ok");
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            weatherDiv.innerHTML = "Current temperature in " + cityForRequest + " is: " + data.main.temp;
+        })
+        .catch(error => {
+            console.error("Error: ", error);
+        })
+}
